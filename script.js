8200538c6849b9ff44fb956f6b5b8dfb23989962
ajok <ajokd6330@gmail.com>
2025-08-31 12:15:59 +0300

    Deleted some code and updated project

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-const h2 = document.createElement("h2");
-h2.textContent= "This content addeded by Javascript";
-document.querySelector("body").appendChild(h2);
+#about {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+#about img {
+  border-radius: 50%;  /* makes it circular */
+  width: 150px;
+  height: 150px;
+  object-fit: cover;
+  box-shadow: 0 4px 8px rgba(95, 35, 35, 0.2);
+}
+
+const elements = document.querySelectorAll('.fade-up');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 50) {
+      el.classList.add('show');
+    }
+  });
+});
+
+
+
+
